f1134c62d6aaa8aabe8890385fdece3275f06dd0
unknown <kkg.rasul@gmail.com>
2021-08-02 15:31:33 +0600

    updated

--- server.js
+++ server.js
@@ -1,4 +1,3 @@
-
 const jsonServer = require('json-server');
 const server = jsonServer.create();
 const router = jsonServer.router('./db.json');
@@ -11,6 +10,16 @@ server.use(jsonServer.rewriter({
   '/api/*': '/$1',
 }))
 server.use(router);
+if (process.env.NODE_ENV === 'production') {
+  server.use(server.static('client/build'));
+  server.get("*", (req, res) => {
+    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
+  });
+} else {
+  server.get('/', (req, res) => {
+    res.send("Api is running");
+  })
+}
 server.listen(PORT, () => {
   console.log('Server is running');
 });
