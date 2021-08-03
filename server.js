ae6e00aa511a55598f246f0b45fe40cb6d115952
unknown <kkg.rasul@gmail.com>
2021-08-03 12:44:29 +0600

    update

--- server.js
+++ server.js
@@ -4,22 +4,12 @@ const router = jsonServer.router('./db.json');
 const middlewares = jsonServer.defaults({
   static: './build'
 });
-const PORT = process.env.PORT || 3000;
+const PORT = process.env.PORT || 8000;
 server.use(middlewares);
 server.use(jsonServer.rewriter({
   '/api/*': '/$1',
 }))
 server.use(router);
-if (process.env.NODE_ENV === 'production') {
-  server.use(server.static('client/build'));
-  server.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, 'client', 'build', 'index.html'));
-  });
-} else {
-  server.get('/', (req, res) => {
-    res.send("Api is running");
-  })
-}
 server.listen(PORT, () => {
   console.log('Server is running');
 });
